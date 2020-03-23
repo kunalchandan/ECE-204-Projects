4ded3426f48c064c3e9fa5abf3f2317ee68ef68d
Kunal Chandan <kunal_chandan@outlook.com>
2020-03-22 21:50:11 -0400

    P4 verified

--- P4/has_converged.h
+++ P4/has_converged.h
@@ -14,38 +14,37 @@ vec<15> f( double t, vec<15> w );
 
 vec<15> f( double t, vec<15> w ) {
     vec<15> dw{0};
+    double h = (b-a)/16;
     for (uint i = 0; i < 15; i ++) {
         if (i == 0) {
             dw[i] = kappa*(ua(t)  - (2*w[i]) + w[i+1]);
-        } else if (i < 14) {
+        } else if (i <= 13) {
             dw[i] = kappa*(w[i-1] - (2*w[i]) + w[i+1]);
         } else {
             dw[i] = kappa*(w[i-1] - (2*w[i]) + ub(t));
         }
-        dw[i] = dw[i]*16*16;
+        dw[i] = dw[i]/h/h;
     }
     return dw;
 }
 
-std::pair<double, uint> err_max(vec<15> w) {
+double err_max(vec<15> w, double diff_err) {
     double err_max{0.0};
     uint index = 0;
     for (uint i = 0; i < 15; i++) {
         double err = ua(0) + (((i+1)/16.0)*(ub(0)-ua(0)));
-        std::cout << err << " ";
         err = std::fabs(w[i] - err);
         if (err > err_max) {
             err_max = err;
             index = i;
         }
     }
-    std::cout << std::endl;
-    return std::make_pair(err_max, index);
+    return err_max - diff_err;
 }
 
 double has_converged( double max_error ) {
     assert( max_error > 0 );
-    double epsilon = max_error*std::pow(10, -3);
+    double epsilon = max_error*std::pow(10, -7);
     // Your implementation here (some hints included):
     // - you can call all globally defined functions in 'data.h'
     // and all globally defined constants
@@ -56,14 +55,49 @@ double has_converged( double max_error ) {
     // u(3.2) returns a vec<15> with the temperatures at the
     // 15 intermediate points at time 3.2
     
-    double t = 0.0;
-    std::pair<double, uint> p = err_max(u(t));
-    while ((p.first > max_error + epsilon) || (p.first < max_error - epsilon)) {
-        // solve e^{-kappa*t} = target
-        p = err_max(u(t));
-        double target = ua(0) + ((p.second/16.0)*ub(0));
-        t += (std::log(u(t)[p.second] + target) - std::log(max_error))/(kappa);
-        std::cout << "mep " << t << std::endl;
+    double tn = 0.0001;
+    double tp = 0.0010;
+    double en = err_max(u(tn), max_error);
+    double ep = err_max(u(tp), max_error);
+
+    double et;
+    while ((ep > max_error + epsilon) || (ep < max_error - epsilon)) {
+        // Solve Secant method
+        // Apply rule
+
+        double tt = ((tn*ep) - (tp*en))/(ep - en);
+        try {
+            // std::cout << "mep " << tt << "  err " << ep.first << std::endl;
+            if (tt != tt) {
+                return tp;
+            }
+            et = err_max(u(tt), max_error);
+        } catch (std::runtime_error r) {
+            return tp;
+        }
+        tn = tp;
+        tp = tt;
+        en = ep;
+        ep = et;
     }
-    return t;
+    return tp;
+}
+
+double convergence( double max_error ) {
+    assert( max_error > 0 );
+    double epsilon = max_error*std::pow(10, -7);
+    // Your implementation here (some hints included):
+    // - you can call all globally defined functions in 'data.h'
+    // and all globally defined constants
+    vec<15> initial_state{};
+    // Initialize your initial state vector.
+    ivp<vec<15>> u{ f, t0, initial_state, H_INIT, H_RANGE, EPS_ABS, vec<15>::norm };
+
+    // u(3.2) returns a vec<15> with the temperatures at the
+    // 15 intermediate points at time 3.2
+    
+    for (double d = 0.1; d < 4.0; d += 0.01) {
+        std::cout << err_max(u(d), 0) << " ";
+    }
+    return 0.0;
 }
--- P4/main.cpp
+++ P4/main.cpp
@@ -9,9 +9,12 @@ int main();
 #include "has_converged.h"
 
 int main() {
-    std::cout.precision( 6 );
+    std::cout.precision( 10 );
     std::clog.precision( 16 );
     std::clog.setstate(std::ios_base::failbit);
-    std::cout << "mep " << has_converged(std::pow(10.0, -6)) << std::endl;
+    for (int x = 0; x < 10; x ++) {
+        std::cout << "10^-"<< x << ", " << has_converged(std::pow(10.0, -x)) << std::endl;
 
+    }
+    // convergence(1);
 }
