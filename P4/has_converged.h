4307234a5b1d9c17857ee0401de0db6dc0de1c6a
Kunal Chandan <kunal_chandan@outlook.com>
2020-03-25 12:47:18 -0400

    P4 Broken, add Bracketed secant

--- P4/has_converged.h
+++ P4/has_converged.h
@@ -1,4 +1,4 @@
-#include "vec.h"
+////// #include "vec.h"
 #include "ivp.h"
 #include "data.h"
 #include <iostream>
@@ -6,6 +6,8 @@
 
 
 // Function declarations
+double bracketed_secant(ivp<vec<15>> u, double max_error, double ep_fac=std::pow(10, -8));
+double secant_method(ivp<vec<15>> u, double max_error, double ep_fac=std::pow(10, -8));
 double has_converged( double max_error );
 vec<15> f( double t, vec<15> w );
 // You will pass 't' to ua(t) and ub(t); for everything else, you
@@ -44,33 +46,94 @@ double err_max(vec<15> w, double diff_err) {
 
 double has_converged( double max_error ) {
     assert( max_error > 0 );
-    double epsilon = max_error*std::pow(10, -7);
     // Your implementation here (some hints included):
     // - you can call all globally defined functions in 'data.h'
     // and all globally defined constants
     vec<15> initial_state{};
+    for (double x = a + ((b-a)/16.0); x < b; x += (b-a)/16.0) {
+        initial_state[x] = u0(x);
+    }
     // Initialize your initial state vector.
     ivp<vec<15>> u{ f, t0, initial_state, H_INIT, H_RANGE, EPS_ABS, vec<15>::norm };
 
     // u(3.2) returns a vec<15> with the temperatures at the
     // 15 intermediate points at time 3.2
-    
+    double t = 0;
+    try {
+        double diff = 0;
+        t = bracketed_secant(u, max_error);
+        diff = t;
+        std::cout << " bs " << t;
+        t = secant_method(u, max_error);
+        std::cout << " sm ";
+        diff -= t;
+        std:: cout << diff << "    diff  " ; 
+    } catch (std::out_of_range o) {
+        std::cout << " bs ";
+    }
+    return t;
+}
+
+double bracketed_secant(ivp<vec<15>> u, double max_error, double ep_fac=std::pow(10, -8)) {
+    double epsilon = max_error*ep_fac;
+    double a{0.0};
+    double b{0.0};
+    double c{0.0};
+    // find lower and upper bound
+    while (err_max(u(b), max_error) > 0.0) {
+        b += 0.1;
+        if (err_max(u(b), max_error) > 0.0) {
+            a = b;
+            c = b;
+        }
+    }
+    // begin bracketed secant
+    double error{err_max(u(c), max_error)};
+    uint conv_count = 0;
+    while ((error > epsilon) || (error < -epsilon)) {
+        // Prevent infinite loop by increasing size of epsilon
+        conv_count++;
+        if (conv_count > 10000) {
+            return bracketed_secant(u, max_error, ep_fac*2);
+        }
+        double ea = err_max(u(a), max_error);
+        double eb = err_max(u(b), max_error);
+
+        c = (a*eb - b*ea)/(eb - ea);
+        error = err_max(u(c), max_error);
+
+        // Update whichever endpoint with same sign
+        if (std::signbit(c) == std::signbit(a)) {
+            a = c;
+        } else {
+            b = c;
+        }
+    }
+    return c;
+}
+
+
+double secant_method(ivp<vec<15>> u, double max_error, double ep_fac=std::pow(10, -8)) {
+    double epsilon = max_error*std::pow(10, -9);
+
     double tn = 0.0001;
     double tp = 0.0010;
     double en = err_max(u(tn), max_error);
     double ep = err_max(u(tp), max_error);
 
     double et;
-    while ((ep > max_error + epsilon) || (ep < max_error - epsilon)) {
+    while ((ep > epsilon) || (ep < -epsilon)) {
         // Solve Secant method
         // Apply rule
 
         double tt = ((tn*ep) - (tp*en))/(ep - en);
         try {
-            // std::cout << "mep " << tt << "  err " << ep.first << std::endl;
-            if (tt != tt) {
+            if (std::isinf(tt)) {
+                throw std::out_of_range("INF");
+            } else if (std::isnan(tt)) {
                 return tp;
             }
+
             et = err_max(u(tt), max_error);
         } catch (std::runtime_error r) {
             return tp;
@@ -90,6 +153,11 @@ double convergence( double max_error ) {
     // - you can call all globally defined functions in 'data.h'
     // and all globally defined constants
     vec<15> initial_state{};
+    for (double x = a + ((b-a)/16.0); x < b; x += (b-a)/16.0) {
+        initial_state[x] = u0(x);
+        // std::cout << initial_state[x] << x << " ";
+    }
+    // std::cout << std::endl;
     // Initialize your initial state vector.
     ivp<vec<15>> u{ f, t0, initial_state, H_INIT, H_RANGE, EPS_ABS, vec<15>::norm };
 
