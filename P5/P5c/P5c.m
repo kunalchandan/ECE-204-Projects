a3e800d4393656e947f9577e0cc510bdda8c5c7f
Kunal Chandan <kunal_chandan@outlook.com>
2020-04-19 15:10:33 -0400

    Complete P5abcd

--- P5/P5c/P5c.m
+++ P5/P5c/P5c.m
@@ -4,4 +4,4 @@ maximize( @sin, 2, 2.5, 3, 1e-5, 1e-5, 10 )
 ##maximize( @sin, 3, 3.5, 4, 1e-5, 1e-5, 10 ) % invalid arguments
 ##maximize( @sin, 4, 4.5, 5, 1e-5, 1e-5, 10 ) % invalid arguments
 ##maximize( @sin, 5, 5.5, 6, 1e-5, 1e-5, 10 ) % invalid arguments
-##maximize( @tanh, 0, 0.5, 1, 1e-5, 1e-5, 10 ) % method did not converge
+maximize( @tanh, 0, 0.5, 1, 1e-5, 1e-5, 10 ) % method did not converge
--- P5/P5c/maximize.m
+++ P5/P5c/maximize.m
@@ -9,18 +9,19 @@ function [x3] = maximize( f, x0, x1, x2, eps_step, eps_abs, max_iter )
   % fit quadratic
   p = polyfit(xs, fxs, 2);
   [a b c] = num2cell(p){:};
-  
+  % find max of quadratic
   x3 = (-1*b)/(2*a);
-  if f(x3) < f(xs(2))
+  % check validity of arguments
+  if f(x3) < f(xs(3))
     error( 'invalid arguments' );
   endif
+  xs = shift(xs, -1);
+  xs(3) = x3;
   
   count = 0;
-  while (abs(x3 - x2) > eps_step) && ...
-        (f(x3) - f(x2) > eps_abs) 
-    count++;
-    if count >= max_iter
-      error( 'method did not converge' );
+  while 1
+    if (abs(xs(3) - xs(2)) < eps_step) && (f(xs(3)) - f(xs(2)) < eps_abs)
+      return;
     endif
     % fit quadratic
     p = polyfit(xs, fxs, 2);
@@ -29,7 +30,10 @@ function [x3] = maximize( f, x0, x1, x2, eps_step, eps_abs, max_iter )
     x3 = (-1*b)/(2*a);
     xs = shift(xs, -1);
     xs(3) = x3;
-  endwhile  
-  
-  
+    fxs = f(xs);
+    count++;
+    if count > max_iter
+      error( 'method did not converge' );
+    endif
+  endwhile
 end
